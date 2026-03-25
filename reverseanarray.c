4bbd695d70b5f8639331039f8afef0e774f3f930
PS1555 <suryavanshiprakhar555@gmail.com>
2026-03-25 14:18:43 +0530

    Add files via upload

---
+++ reverseanarray.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, temp;
+    // Input size
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    // Input array elements
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    // Reversing the array
+    for(i = 0; i < n/2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+    // Output reversed array
+    printf("Reversed array:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
